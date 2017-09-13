87df0fd0927fa4fa9c73eaff67ab8dc0cf30b62e
Cacek <you@example.com>
2017-09-13 14:37:56 +0200

    poprawki do 6.2

--- src/test/java/com/kodilla/testing/TestingMain.java
+++ src/test/java/com/kodilla/testing/TestingMain.java
@@ -1,36 +1,7 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int sum = calculator.add(2, 2);
-
-        if (sum == 4) {
-            System.out.println("2 + 2 = 4 - ok");
-        } else {
-            System.out.println("Error!");
-        }
-
-        int sub = calculator.subtract(2, 2);
-
-        if (sub == 0) {
-            System.out.println("2 - 2 = 0 - ok");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
