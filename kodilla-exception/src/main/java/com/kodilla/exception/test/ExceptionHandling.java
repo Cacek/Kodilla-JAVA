65909b4e20f4fd6f144e45073d36e6317df200c3
Your Name <Cacek@users.noreply.github.com>
2017-09-27 19:05:53 +0200

    Zadanie 8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+             System.out.println(secondChallenge.probablyIWillThrowException(3.0, 1.0));
+
+        } catch (Exception e) {
+
+            System.out.println("Zlapany wyjatek");
+
+        }
+
+
+
+    }
+
+}
